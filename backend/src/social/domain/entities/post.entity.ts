b97b34145de0cc4ba18777bd3c8fe2292a67ee10
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-10-01 10:46:54 +0530

    git

--- backend/src/social/domain/entities/post.entity.ts
+++ backend/src/social/domain/entities/post.entity.ts
@@ -28,3 +28,20 @@ export class PostWithLikeStatus {
     public mediaFiles?: string[], // New field for multiple files
   ) {}
 }
+
+// export class PostWithLikeAndUser {
+//   constructor(
+//     public id: string,
+//     public user: string,
+//     public content?: string,
+//     public likeCount: number = 0,
+//     public commentCount: number = 0,
+//     public userLiked: boolean = false, // Domain property
+//     public createdAt?: string,
+//     public updatedAt?: string,
+//     public image?: string, // Keep for backward compatibility
+//     public mediaFiles?: string[], // New field for multiple files
+//     public userFirstName?: string,
+//     public userLastName?: string,
+//   ) {}
+// }
