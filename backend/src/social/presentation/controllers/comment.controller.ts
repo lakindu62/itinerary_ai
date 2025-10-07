61faa467a53af7290dbe1716700e0d36e030ca30
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-10-08 01:34:18 +0530

    like and comment controllers migrated to use ClerkAuthGuard

--- backend/src/social/presentation/controllers/comment.controller.ts
+++ backend/src/social/presentation/controllers/comment.controller.ts
@@ -8,11 +8,18 @@ import {
   Logger,
   Param,
   Post,
+  UnauthorizedException,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { CreateCommentDto } from '@shared/types/social/create-comment.dto';
+import { ClerkAuthGuard } from 'src/shared/guards/clerk-auth-guard';
 
 import { CommentService } from 'src/social/application/services/comment.service';
 
+import { Request } from 'express';
+
+@UseGuards(ClerkAuthGuard) //Note that the ClerkAuthGuard is applied to all endpoints in the file. Change if needed - Amzal
 @Controller('social/posts/:postId/comments')
 export class CommentController {
   private readonly logger = new Logger(CommentController.name);
@@ -31,10 +38,15 @@ export class CommentController {
   @Post()
   async addComment(
     @Param('postId') postId: string,
-    @Body() body: { user?: string; content?: string },
+    @Body() body: { content?: string },
+    @Req() req: Request,
   ) {
+    if (!req.user?._id) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
     const dto: CreateCommentDto = {
-      user: body?.user as string,
+      user: req.user._id,
       post: postId,
       content: body?.content as string,
     };
@@ -50,9 +62,12 @@ export class CommentController {
   async deleteComment(
     @Param('postId') postId: string,
     @Param('commentId') commentId: string,
-    @Body() body: { user?: string },
+    @Req() req: Request,
   ) {
-    const userId = body?.user as string;
+    if (!req.user?._id) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+    const userId = req.user._id;
     this.logger.log(`DELETE /posts/:postId/comments/:commentId request`, {
       userId,
       postId,
